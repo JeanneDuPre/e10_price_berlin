0dbb788101dac9770be39d745983ede386929036
Janine Wiesemann <119901184+JeanneDuPre@users.noreply.github.com>
2023-10-18 23:29:14 +0200

    Update find_the_cheapest_gas_station_in_your_area.py

--- find_the_cheapest_gas_station_in_your_area.py
+++ find_the_cheapest_gas_station_in_your_area.py
@@ -19,9 +19,10 @@ else:
 # Fetch new data
 new_data = pytankerkoenig.getNearbyStations(API_KEY, '52.4819479', '13.3213254', '4', 'all', 'dist')
 new_data['scraping_datetime'] = current_time  # Add a column with scraping datetime
+new_data_df = pd.DataFrame(new_data)
 
 # Append the new data to the existing dataset
-df_combined = pd.concat([df_existing, new_data], ignore_index=True)
+df_combined = pd.concat([df_existing, new_data_df], ignore_index=True)
 
 # Save the updated dataset
 with open(existing_data_file, "w") as outfile:
