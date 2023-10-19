56993cc37352efe0d2219c8e1b6097d12a8a2800
Janine Wiesemann <119901184+JeanneDuPre@users.noreply.github.com>
2023-10-19 22:44:47 +0200

    Update find_the_cheapest_gas_station_in_your_area.py

--- find_the_cheapest_gas_station_in_your_area.py
+++ find_the_cheapest_gas_station_in_your_area.py
@@ -24,6 +24,10 @@ new_data_df = pd.DataFrame(new_data)
 # Append the new data to the existing dataset
 df_combined = pd.concat([df_existing, new_data_df], ignore_index=True)
 
+# # Save the updated dataset
+# with open(existing_data_file, "w") as outfile:
+#     outfile.write(df_combined.to_json(orient='records', lines=True))
 # Save the updated dataset
 with open(existing_data_file, "w") as outfile:
-    outfile.write(df_combined.to_json(orient='records', lines=True))
+    for _, row in df_combined.iterrows():
+        outfile.write(json.dumps(row.to_dict()) + '\n')
